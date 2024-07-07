8b7d064098039d0b6b1da750198d9dc138e6116b
prograin <ahmadreza.pcg1377@gmail.com>
2024-07-07 13:18:30 +0330

    change matrix Pers and add QTableView

--- src/item/element/matrix_perspective.py
+++ src/item/element/matrix_perspective.py
@@ -5,6 +5,53 @@ from ..qtc.QtCustom import *
 
 import numpy as np
 
+
+class GeneratorsMatrix(QWidget):
+
+    def __init__(self, parent):
+        super().__init__(parent)
+
+        self.createLay()
+        self.createWgt()
+        self.assemblyWgt()
+        self.setPro()
+
+    def createWgt(self):
+        self.main_wgt = QWidget()
+
+        self.generate_la = QLabel('Generators')
+
+        self.identity_pb = QPushButton('Identity', self)
+        self.random_pb = QPushButton('Random', self)
+        self.utr_pb = QPushButton('Upper Tria', self)
+        self.ltr_pb = QPushButton('Lower Tria', self)
+
+    def createLay(self):
+        self.v_cont_l = QVBoxLayout()
+        self.h_cont_l = QHBoxLayout()
+
+    def assemblyWgt(self):
+        self.h_cont_l.addWidget(self.identity_pb)
+        self.h_cont_l.addWidget(self.random_pb)
+        self.h_cont_l.addWidget(self.utr_pb)
+        self.h_cont_l.addWidget(self.ltr_pb)
+
+        self.v_cont_l.addWidget(self.generate_la)
+        self.v_cont_l.addLayout(self.h_cont_l)
+
+        self.setLayout(self.v_cont_l)
+
+    def setPro(self):
+        self.ltr_pb.setObjectName('GENE_LOWER_TRIA')
+        self.utr_pb.setObjectName('GENE_UPPER_TRIA')
+        self.random_pb.setObjectName('GENE_RANDOM')
+        self.identity_pb.setObjectName('GENE_IDENTITY')
+
+        self.v_cont_l.setContentsMargins(0, 0, 0, 0)
+
+        self.generate_la.setAlignment(Qt.AlignmentFlag.AlignCenter)
+
+
 "________________________________________________________________________________"
 "Row X Column"
 "________________________________________________________________________________"
@@ -78,9 +125,20 @@ class MatrixFieldEdit(QTableView):
         vertical_head.setSectionResizeMode(QHeaderView.ResizeMode.ResizeToContents)
         horizontal_head.setSectionResizeMode(QHeaderView.ResizeMode.ResizeToContents)
 
-    def createField(self, row_count, column_count):
-        self.standard_model.setRowCount(row_count)
-        self.standard_model.setColumnCount(column_count)
+    def createField(self, row_count=None, column_count=None, array: np.ndarray = None):
+        if isinstance(array, np.ndarray):
+            row_count, column_count = array.shape
+            self.standard_model.setRowCount(row_count)
+            self.standard_model.setColumnCount(column_count)
+
+            for row_index in range(row_count):
+                for column_index in range(column_count):
+                    value = array[row_index, column_index]
+                    index = self.standard_model.index(row_index, column_index)
+                    self.standard_model.setData(index, str(value), Qt.ItemDataRole.DisplayRole)
+        else:
+            self.standard_model.setRowCount(row_count)
+            self.standard_model.setColumnCount(column_count)
 
     def getMatrixData(self):
         matrix_array = []
@@ -111,6 +169,7 @@ class MatrixPerspective(QWidget):
         self.max_error_value = QLabel(" Size Matrix is more than 40 \n althogh you can use it for render ")
         self.matrix_field_edit = MatrixFieldEdit(self)
         self.size_mat_wgt = SizeMatrixWgt(self)
+        self.generators_wgt = GeneratorsMatrix(self)
 
     def createLay(self):
         self.v_cont_l = QVBoxLayout()
@@ -119,6 +178,7 @@ class MatrixPerspective(QWidget):
         self.v_cont_l.addWidget(self.matrix_field_edit)
         self.v_cont_l.addWidget(self.max_error_value, alignment=Qt.AlignmentFlag.AlignCenter)
         self.v_cont_l.addWidget(self.size_mat_wgt, alignment=Qt.AlignmentFlag.AlignBottom)
+        self.v_cont_l.addWidget(self.generators_wgt, alignment=Qt.AlignmentFlag.AlignBottom)
 
         self.setLayout(self.v_cont_l)
 
@@ -136,6 +196,10 @@ class MatrixPerspective(QWidget):
         self.v_cont_l.setSpacing(0)
 
     def getElement(self):
+        self.generate_ltri = self.findChild(QPushButton, 'GENE_LOWER_TRIA')
+        self.generate_utri = self.findChild(QPushButton, 'GENE_UPPER_TRIA')
+        self.generate_random = self.findChild(QPushButton, 'GENE_RANDOM')
+        self.generate_identity = self.findChild(QPushButton, 'GENE_IDENTITY')
         self.column_sb: QSpinBox = self.size_mat_wgt.getColumnSb()
         self.row_sb: QSpinBox = self.size_mat_wgt.getRowSb()
 
@@ -143,6 +207,7 @@ class MatrixPerspective(QWidget):
         return self.matrix_field_edit.getMatrixData()
 
     def connectSignalSlot(self):
+        self.generate_identity.clicked.connect(lambda x: self.on_generates('identity'))
         self.row_sb.editingFinished.connect(lambda: self.on_matrix_size_change(self.row_sb.value(), self.column_sb.value()))
         self.column_sb.editingFinished.connect(lambda: self.on_matrix_size_change(self.row_sb.value(), self.column_sb.value()))
 
@@ -150,7 +215,16 @@ class MatrixPerspective(QWidget):
         if row_count + column_count > 40:
             self.matrix_field_edit.setHidden(True)
             self.max_error_value.setHidden(False)
+            self.matrix_field_edit.createField(row_count, column_count)
         else:
             self.matrix_field_edit.setVisible(True)
             self.max_error_value.setHidden(True)
             self.matrix_field_edit.createField(row_count, column_count)
+
+    def on_generates(self, type_):
+        row_count = self.row_sb.value()
+        column_count = self.column_sb.value()
+
+        if type_ == 'identity':
+            I = np.array([[1 if i == j else 0 for j in range(row_count)] for i in range(column_count)])
+            self.matrix_field_edit.createField(array=I)
